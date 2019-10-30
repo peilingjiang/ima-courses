82281677091605f5a513e85a9b3250a82b1ce201
Peiling Jiang <jiangpeiling98@outlook.com>
2019-10-30 03:07:38 -0400

    update and fix gitignore

--- f19-ml-art/two-tales-of-one-city/public/sketch.js
+++ f19-ml-art/two-tales-of-one-city/public/sketch.js
@@ -1,4 +1,3 @@
-let modelCounter = 1; // Count model
 let bg;
 let runningInference = false;
 let generated = false;
@@ -10,23 +9,31 @@ function preload() {
 
 function setup() {
   noCanvas();
-  zh_nameRNN = ml5.charRNN('./models/zh_name/', modelReady);
-  zh_contentRNN = ml5.charRNN('./models/zh_description/', modelReady);
-  en_nameRNN = ml5.charRNN('./models/en_name/', modelReady);
-  en_contentRNN = ml5.charRNN('./models/en_description/', modelReady);
+  zh_nameRNN = ml5.charRNN('./models/zh_name/', modelReady_zhn);
+  zh_contentRNN = ml5.charRNN('./models/zh_description/', modelReady_zhc);
+  en_nameRNN = ml5.charRNN('./models/en_name/', modelReady_enn);
+  en_contentRNN = ml5.charRNN('./models/en_description/', modelReady_enc);
   noLoop();
 }
 
-function modelReady() {
-  console.log(str(modelCounter) + ' Model Ready');
-  modelCounter++;
-  if (!generated && modelCounter === 4) {
-    generate(zh_nameRNN, ['zh', 'name']);
-    generate(en_nameRNN, ['en', 'name']);
-    generate(zh_contentRNN, ['zh', 'content']);
-    generate(en_contentRNN, ['en', 'content']);
-    generated = true;
-  }
+function modelReady_zhn() {
+  console.log('ZH NAME Model Ready');
+  generate(zh_nameRNN, ['zh', 'name']);
+}
+
+function modelReady_enn() {
+  console.log('EN NAME Model Ready');
+  generate(en_nameRNN, ['en', 'name']);
+}
+
+function modelReady_zhc() {
+  console.log('ZH CONTENT Model Ready');
+  generate(zh_contentRNN, ['zh', 'content']);
+}
+
+function modelReady_enc() {
+  console.log('EN CONTENT Model Ready');
+  generate(en_contentRNN, ['en', 'content']);
 }
 
 function draw() {
@@ -39,7 +46,7 @@ function generate(rnn, type) {
   let seed = get_seed(type);
 
   let data = {
-    seed: seed,
+    seed: seed.slice(-1),
     temperature: 0.5,
     length: get_len(type)
   };
@@ -59,6 +66,7 @@ function update_text(t, r) {
     } else {
       $("#zh_content").text(r + "......");
       $("#zh_content").append("&nbsp;&nbsp;<em>[了解更多]</em>");
+      $("#zh_window").css('height', 'auto');
     }
   } else {
     let lastSpaceInd = r.lastIndexOf(' ');
@@ -68,13 +76,14 @@ function update_text(t, r) {
     } else {
       $("#en_content").text(r + "...");
       $("#en_content").append("&nbsp;&nbsp;<em>[READ MORE]</em>")
+      $("#en_window").css('height', 'auto');
     }
   }
 }
 
 function get_len(t) {
   if (t[0] == 'zh' && t[1] == 'name') {
-    return int(random(12, 20));
+    return int(random(12, 17));
   } else if (t[0] == 'zh' && t[1] == 'content') {
     return int(random(150, 250));
   } else if (t[0] == 'en' && t[1] == 'name') {
@@ -86,8 +95,8 @@ function get_len(t) {
 
 function get_seed(t) {
   if (t[0] == 'zh') {
-    return random(['中国', '香港', '']);
+    return random(['中国', '香港', '', '暴徒']);
   } else if (t[0] == 'en') {
-    return random(['Hong Kong ', 'China ', '']);
+    return random(['Hong Kong ', 'China ', '', 'Protest ']);
   }
 }
--- f19-ml-art/two-tales-of-one-city/public/windows.js
+++ f19-ml-art/two-tales-of-one-city/public/windows.js
@@ -1,6 +1,20 @@
-function draw_bg() {
-  // TODO: draw background images
-}
+// 重写 / Refake
+$('button').click(function (event) {
+  event.preventDefault();
+  if (event.target.id == 'zh_windowheaderbutton') {
+    $("#zh_title").text('重新杜撰中');
+    $("#zh_content").text("重新杜撰中");
+    $("#zh_window").css('height', '80vh');
+    generate(zh_nameRNN, ['zh', 'name']);
+    generate(zh_contentRNN, ['zh', 'content']);
+  } else if (event.target.id == 'en_windowheaderbutton') {
+    $("#en_title").text('ReFAKING');
+    $("#en_content").text("ReFAKING");
+    $("#en_window").css('height', '80vh');
+    generate(en_nameRNN, ['en', 'name']);
+    generate(en_contentRNN, ['en', 'content']);
+  }
+})
 
 // Make the DIV element draggable:
 dragElement(document.getElementById("zh_window"));
@@ -11,9 +25,15 @@ function dragElement(elmnt) {
   if (document.getElementById(elmnt.id + "header")) {
     // if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    // document.getElementById(elmnt.id + "header").onmouseup = dragMouseUp;
+    document.getElementById(elmnt.id + "headertext").onmouseup = dragMouseDown;
+    // document.getElementById(elmnt.id + "headertext").onmouseup = dragMouseUp;
+    document.getElementById(elmnt.id + "headerbutton").onmouseup = dragMouseDown;
+    // document.getElementById(elmnt.id + "headerbutton").onmouseup = dragMouseUp;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
+    // elmnt.onmouseup = dragMouseUp;
   }
 
   function dragMouseDown(e) {
@@ -25,24 +45,54 @@ function dragElement(elmnt) {
     document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
-    if (e.target.id == 'zh_windowheader') {
+    if (e.target.id == 'zh_windowheader' || e.target.id == 'zh_windowheadertext' || e.target.id == 'zh_windowheaderbutton') {
       document.getElementById("zh_window").style.zIndex = "3";
       document.getElementById("zh_windowheader").style.zIndex = "4";
       document.getElementById("en_window").style.zIndex = "1";
       document.getElementById("en_windowheader").style.zIndex = "2";
-      change_bg();
-    } else if (e.target.id == 'en_windowheader') {
+      if (e.target.id != 'zh_windowheaderbutton') {
+        change_bg();
+      }
+      // load_bg();
+    } else if (e.target.id == 'en_windowheader' || e.target.id == 'en_windowheadertext' || e.target.id == 'en_windowheaderbutton') {
       document.getElementById("en_window").style.zIndex = "3";
       document.getElementById("en_windowheader").style.zIndex = "4";
       document.getElementById("zh_window").style.zIndex = "1";
       document.getElementById("zh_windowheader").style.zIndex = "2";
-      change_bg();
+      if (e.target.id != 'en_windowheaderbutton') {
+        change_bg();
+      }
+      // load_bg();
     }
   }
 
+  function dragMouseUp(e) {
+    // Do nothing here
+  }
+
   function change_bg() {
-    console.log("url('" + random(imgArray) + "')");
-    $('#background_image').css('background-image', "url('" + random(imgArray) + "')");
+    let lastImgUrl = '';
+    let imgUrl;
+    imgUrl = random(imgArray);
+    if (lastImgUrl == '') {
+      // Init
+      lastImgUrl = imgUrl;
+    } else {
+      // Avoid same image twice
+      while (imgUrl == lastImgUrl) {
+        imgUrl = random(imgArray);
+      }
+      lastImgUrl = imgUrl;
+    }
+    let bgImg = new Image();
+    bgImg.src = imgUrl;
+    $(bgImg).on('load', function () {
+      $('#background_image').css('background-image', "url('" + $(this).attr("src") + "')");
+    });
+  }
+
+  function load_bg() {
+    // Do nothing here
   }
 
   function elementDrag(e) {
